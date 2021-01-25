6f7a69dc16055032b9a59d7ba95e9bb02cf8ec9b
Nikolay Malov <n.malov92@gmail.com>
2021-01-26 00:05:45 +0300

    homework. Try 1

--- src/main/java/ru/hh/school/entity/Employer.java
+++ src/main/java/ru/hh/school/entity/Employer.java
@@ -4,20 +4,39 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 //TODO: оформите entity
+@Entity
+@Table(name = "employer")
 public class Employer {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "employer_id")
   private Integer id;
 
+  @Column(name = "company_name")
   private String companyName;
 
   // не используйте java.util.Date
   // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#basic-datetime-java8
+  @Column(name = "creation_time")
   private LocalDateTime creationTime;
 
+  @OneToMany(mappedBy = "employer")
+  @Cascade(CascadeType.SAVE_UPDATE)
   private List<Vacancy> vacancies = new ArrayList<>();
 
+  @Column(name = "block_time")
   private LocalDateTime blockTime;
 
   public List<Vacancy> getVacancies() {
@@ -50,8 +69,12 @@ public class Employer {
   // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
     Employer employer = (Employer) o;
     return Objects.equals(companyName, employer.companyName);
   }
